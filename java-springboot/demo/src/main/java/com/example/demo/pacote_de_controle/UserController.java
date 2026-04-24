38cf6bdb2dc003e3fdc66132bd40317281b65ab4
Julio <julyoviny@gmail.com>
2026-04-24 11:48:00 -0300

    alteraçao no texto na hora de rodar

--- java-springboot/demo/src/main/java/com/example/demo/pacote_de_controle/UserController.java
+++ java-springboot/demo/src/main/java/com/example/demo/pacote_de_controle/UserController.java
@@ -1,5 +1,13 @@
 package com.example.demo.pacote_de_controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
-    
+
+    @GetMapping("/")
+    public String home() {
+        return "Servidor rodando! Esse é o meu backend em Spring Boot.";
+    }
 }
